cf489d34efe05ec343bed4d7b8af746752e0af11
samantha <samdurdy@hotmail.co.uk>
2022-12-06 17:37:36 +0000

    Created script to add service to nginx proxty

---
+++ add_new_service_to_nginx_proxy.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Prompt the user for the text that they want to insert
+read -p "What is the IP address of the machine on which the webapp is running (if it is running on this machine please enter 127.0.0.1): " ip
+
+ip_regex="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+if grep -Evq "$ip_regex" <<< "$ip"; then
+    echo "Was not given a valid IP address, please try again"
+    exit 1
+fi
+
+read - p "What port was the webapp running on?" port
+
+if !  [[ "$port" -gt 0 && "$port" -lt 65536 ]]; then
+    echo "Invalid port number"
+    exit 1
+fi
+
+read -p "Enter address after the slash for example if I wanted to launch lmds.liv.ac.uk/MOF_ML I would enter \"MOF_ML\"" directory
+
+answer=""
+while [[ "$answer" != y && "$answer" != "n"]]; do
+    read -p "Is your nginx config in the default directory (/etc/nginx/sites-available/default)  (y/n)?" answer
+    
+  if [[ "$answer" == "y" ]]; then
+    nginx_config="/etc/nginx/sites-available/default"
+  elif [[ "$answer" == "n" ]]; then
+    # Do something if the user answered "n"
+    read -p "Please enter the address of your nginx config" nginx_config
+  else
+
+    echo "Please answer 'y' for yes or 'n' for no."
+done
+
+text="
+
+    location $directory {
+        proxy_pass http://$ip:$port/;
+    }
+
+"
+current_date=$(date +"%d.%m.%Y")
+backup_location="$nginx_config.bak.$current_date"
+echo "creating backup of current config at $backup_location"
+sudo cp $nginx_config $backup_location
+# Use sed to insert the text after the specified pattern
+sudo sed -i "/location \/static {(?:[^{]*|{[^{]*})*}/i $text" nginx_config
+
+systemctl restart nginx
